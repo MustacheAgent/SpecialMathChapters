6e15f95fa3fae3c62831f1eeaaadf2e4e3d1d3f3
MustacheAgent <dimonus98@yandex.ru>
2021-12-15 16:08:13 +0300

    новый проект

---
+++ GoldCodes/GoldCodes/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace GoldCodes.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        #region поля
+        private int _invalidate;
+        #endregion
+
+        #region свойства
+        public int Invalidate
+        {
+            get => _invalidate;
+            set
+            {
+                _invalidate = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+    }
+}
