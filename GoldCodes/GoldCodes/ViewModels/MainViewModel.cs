f8c2e155e103ff4b64347e89963b70e4d5ebc294
MustacheAgent <dimonus98@yandex.ru>
2021-12-29 19:25:14 +0300

    исследование

--- GoldCodes/GoldCodes/ViewModels/MainViewModel.cs
+++ GoldCodes/GoldCodes/ViewModels/MainViewModel.cs
@@ -24,12 +24,14 @@ namespace GoldCodes.ViewModels
         private int _sampleRate;
         private int _carryFreq;
         private int _snr;
+        private int _repeats;
+        private int _beginSNR;
+        private int _endSNR;
+        private int _stepSNR;
 
         private string _binaryData;
         private string _decodedData;
         private double _successProbability;
-
-        private double beta;
         #endregion
 
         #region массивы
@@ -123,6 +125,42 @@ namespace GoldCodes.ViewModels
                 OnPropertyChanged();
             }
         }
+        public int Repeats
+        {
+            get => _repeats;
+            set
+            {
+                _repeats = value;
+                OnPropertyChanged();
+            }
+        }
+        public int BeginSNR
+        {
+            get => _beginSNR;
+            set
+            {
+                _beginSNR = value;
+                OnPropertyChanged();
+            }
+        }
+        public int EndSNR
+        {
+            get => _endSNR;
+            set
+            {
+                _endSNR = value;
+                OnPropertyChanged();
+            }
+        }
+        public int StepSNR
+        {
+            get => _stepSNR;
+            set
+            {
+                _stepSNR = value;
+                OnPropertyChanged();
+            }
+        }
         #endregion
 
         Random random;
@@ -133,8 +171,10 @@ namespace GoldCodes.ViewModels
         public List<DataPoint> GoldCorrelation2 { get; set; }
         public List<DataPoint> GoldCorrelation3 { get; set; }
         public List<DataPoint> GoldCorrelation4 { get; set; }
+        public List<DataPoint> Research { get; set; }
 
         public ICommand Modulate { get; set; }
+        public ICommand StartResearch { get; set; }
 
         public MainViewModel()
         {
@@ -145,8 +185,10 @@ namespace GoldCodes.ViewModels
             CarryFrequency = 6200;
             SNR = 100;
             BinaryData = "0x27";
-
-            beta = 0.9;
+            BeginSNR = 10;
+            EndSNR = -10;
+            Repeats = 100;
+            StepSNR = 1;
 
             ComponentI = new List<DataPoint>();
             ComponentQ = new List<DataPoint>();
@@ -154,6 +196,8 @@ namespace GoldCodes.ViewModels
             GoldCorrelation2 = new List<DataPoint>();
             GoldCorrelation3 = new List<DataPoint>();
             GoldCorrelation4 = new List<DataPoint>();
+
+            Research = new List<DataPoint>();
 
             random = new Random();
 
@@ -185,10 +229,18 @@ namespace GoldCodes.ViewModels
                 decs[3].ToPoints(GoldCorrelation4);
                 Invalidate++;
             });
+
+            StartResearch = new RelayCommand(o =>
+            {
+                ResearchSNR();
+                Invalidate++;
+            });
         }
 
         private void GetSignal()
         {
+            bits = Bit.FromString(BinaryData);
+            changed_bits = Sequences.GoldTransform(bits, gold);
             double[][] result = Calculation.PM4(changed_bits, SampleRate, CarryFrequency);
             I = result[0];
             Q = result[1];
@@ -201,7 +253,7 @@ namespace GoldCodes.ViewModels
             }
         }
 
-        private void Experiment(int CountsPerBit = 1)
+        private int Experiment(bool thread = false, int CountsPerBit = 1)
         {
             for (int i = 0; i < decs.Length; i++)
             {
@@ -233,10 +285,35 @@ namespace GoldCodes.ViewModels
             {
                 right_bits += bits[i] == decoded_bits[i] ? 1 : 0;
             }
-            Success = (double)right_bits / bits.Length * 100;
 
-            DecodedData = Bit.ToString(decoded_bits, 16);
+            double success = (double)right_bits / bits.Length * 100;
+            if (!thread) Success = success;
 
+            string decoded = Bit.ToString(decoded_bits, 16);
+            if (!thread) DecodedData = decoded;
+            return right_bits;
+        }
+
+        private void ResearchSNR()
+        {
+            Research.Clear();
+            int totalBits = bits.Length * Repeats, rightBits;
+            double success;
+
+            for (int i = BeginSNR; i >= EndSNR; i -= StepSNR)
+            {
+                success = 0;
+                for (int k = 0; k < Repeats; k++)
+                {
+                    noisedI = Calculation.AddNoise(I, i, random);
+                    noisedQ = Calculation.AddNoise(Q, i, random);
+                    noisedFi = Calculation.AddNoise(fi, i, random);
+
+                    rightBits = Experiment(true);
+                    success += (double)rightBits / totalBits;
+                }
+                Research.Add(new DataPoint(i, success));
+            }
         }
     }
 }
